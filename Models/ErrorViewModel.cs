2e80daac227021675f3bdecb95c58a49d69e29a3
Jakub Zięba <jakubzieba7@gmail.com>
2023-10-05 21:49:03 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdvertisingPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
