4d10573fe7779f414877f96edf8c698ca380ae91
Jakub Zięba <jakubzieba7@gmail.com>
2023-10-07 10:17:37 +0200

    Added ApplicationUser, replaced all IdentityUser and prepare project to successful add migration and update database actions

---
+++ Core/Models/Domains/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AdvertisingPortal.Core.Models.Domains
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
--- Persistence/ApplicationDbContext.cs
+++ Persistence/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using AdvertisingPortal.Core.Models.Domains;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace AdvertisingPortal.Persistence
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
