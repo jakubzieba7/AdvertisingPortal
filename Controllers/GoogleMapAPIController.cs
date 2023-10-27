6555748d645cdb574b5cc847c4025460e98683b5
Jakub Zięba <jakubzieba7@gmail.com>
2023-10-27 23:31:02 +0200

    Added sketch of Google Map API

---
+++ Controllers/GoogleMapAPIController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdvertisingPortal.Controllers
+{
+    public class GoogleMapAPIController : Controller
+    {
+        public IActionResult Map()
+        {
+            return View();
+        }
+    }
+}
